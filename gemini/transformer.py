19fef6fcc2d4450cd9a8a32372c288c26ca7d6c9
albert.shi <heng.shi@enflame-tech.com>
2021-02-08 17:15:16 +0800

    add a node transformer base, untested

---
+++ gemini/transformer.py
@@ -0,0 +1,11 @@
+import ast
+
+__all__ = ['Transformer']
+
+class Transformer(ast.NodeTransformer):
+
+    # hahaa
+    def visit_Add(self, node):
+        if node.id == 'Add':
+            return ast.Sub()
+        return node
