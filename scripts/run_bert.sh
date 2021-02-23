9af0779d97a9eb62aff6d2648a4e8a1611a40fc7
albert.shi <heng.shi@enflame-tech.com>
2021-02-23 15:41:42 +0800

    add scripts to download pretrainted models

--- scripts/run_bert.sh
+++ scripts/run_bert.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
+get_sha256sum() {
+  cat $1 | sha256sum | head -c 64
+}
+
+string_contains () {
+  [ -z "$1"  ] || { [ -z "${2##*$1*}"  ] && [ -n "$2"  ]; };
+}
+
+download_file() {
+  if [ -f "$1" ]; then
+    echo "$1 exist"
+    # check checksum
+    if string_contains $2 `get_sha256sum $1`; then
+      echo "found correct file, skip download"
+      return 0
+    fi
+    echo "found file, but filehash failed, do download"
+  else
+    echo "not found file, do download"
+  fi
+  rm -f $1
+  if string_contains "24_H-1024" $1; then
+    wget https://storage.googleapis.com/bert_models/2019_05_30/wwm_uncased_L-24_H-1024_A-16.zip
+  elif string_contains "12_H-768" $1; then
+    wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
+  else
+    echo "invalid filename specified, cannot process"
+  fi
+  echo "clean $1"
+  echo "download $1"
+  return 0
+}
+
 script_path=`dirname $0`
 script_realpath=`realpath $script_path`
-echo $script_realpath
+echo "script path = "$script_realpath
 top_dir_path=$script_path"/.."
 top_dir_realpath=`realpath $top_dir_path`
-echo $top_dir_realpath
+echo "top dir = "$top_dir_realpath
 bert_dir=$top_dir_realpath"/external/TopsModels/research/training/tensorflow/nlp/bert"
-echo $bert_dir
+echo "bert dir = "$bert_dir
 
 # download pretrained model, and dataset
 mkdir -p $bert_dir"/pretrained_models"
@@ -15,29 +48,55 @@ mkdir -p $bert_dir"/dataset"
 
 # download pretrained model
 cd $bert_dir"/pretrained_models"
+FILE=wwm_uncased_L-24_H-1024_A-16.zip
+FILE_PATH=$bert_dir"/pretrained_models"$FILE
+FILE_HASH="3619df03637b027f41f4bf6d3ea21d4f162129dd57b6cc223939a320046a1ec5"
+download_file $FILE $FILE_HASH
+FILE2=uncased_L-12_H-768_A-12.zip
+FILE_PATH2=$bert_dir"/pretrained_models"$FILE2
+FILE_HASH2="d15224e1e7d950fb9a8b29497ce962201dff7d27b379f5bfb4638b4a73540a04"
+download_file $FILE2 $FILE_HASH2
+cd -
 
-FILE=uncased_L-24_H-1024_A-16.zip
-# move to function
-if [ -f "$FILE" ]; then
-  echo "$FILE exist"
-  # check checksum
-  # if ok, skip
-  # if not ok, goto clean and download
-else
-  echo "$FILE not exist"
-  # clean and download
-fi
+# download dataset
+cd $bert_dir"/dataset"
 
+
+cd -
+
+export PYTHONPATH="${PYTHONPATH}:${top_dir_realpath}:${bert_dir}"
+export BERT_LARGE=$bert_dir"/pretrained/uncased_L-24_H-1024_A-16"
+export BERT_Base=$bert_dir"/pretrained/uncased_L-12_H-768_A-12"
+cd $top_dir_realpath"/external/TopsModels/common/"
+python setup.py install
+cd -
 exit -1
-wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-24_H-1024_A-16.zip
-wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
+export ENABLE_INIT_ON_CPU=1
 
-cd $top_dir_realpath
+#export SQUAD_DIR=$BASE_DIR/dataset/squad/v1.1
+#export OUT_DIR=$BASE_DIR/squad_base
+#python run_squad.py \
+  #  --vocab_file=$BERT_BASE/vocab.txt \
+  #  --bert_config_file=$BERT_BASE/bert_config.json \
+  #  --init_checkpoint=$BERT_BASE/bert_model.ckpt \
+  #  --do_train=True \
+  #  --do_predict=True \
+  #  --device=dtu \
+  #  --train_file=$SQUAD_DIR/train-v1.1.json \
+  #  --predict_file=$SQUAD_DIR/dev-v1.1.json \
+  #  --train_batch_size=8\
+  #  --learning_rate=5e-6 \
+  #  --num_train_epochs=2.0 \
+  #  --max_seq_length=384 \
+  #  --doc_stride=128 \
+  #  --output_dir=$OUT_DIR \
+  #  --use_resource=False \
+  #  --use_xla=False \
+  #  --horovod=False \
+  #  --display_loss_steps=100
 # need probe
 
 # set pythonpath
-export PYTHONPATH="${PYTHONPATH}:${top_dir_realpath}:${bert_dir}"
-export BERT_BASE=$bert_dir/uncased_L-24_H-1024_A-16
 
 
 
