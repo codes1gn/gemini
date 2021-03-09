b1a4f9202402dda50219a50fca14d22a6a06170e
albert.shi <heng.shi@enflame-tech.com>
2021-03-09 17:40:42 +0800

    refactor on monad and api_wrapper design

---
+++ gemini/plugins/api_wrapper.py
@@ -0,0 +1,39 @@
+
+from gemini.utils import *
+from .monad import MonadicTensor
+
+
+def reduce_unary_op(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        # FIXME assump the input tensor is the first positional arguments in
+        # all tf.op design
+        m_tensor = MonadicTensor(args[0])
+        print("in reduce_unary_op")
+        print(m_tensor)
+        result = m_tensor.reduce(f, *args[1:], **kwargs)
+        return result.get()
+    return wrapper
+
+
+def bind_unary_op(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        # FIXME assump the input tensor is the first positional arguments in
+        # all tf.op design
+        m_tensor = MonadicTensor(args[0])
+        result = m_tensor.bind(f, *args[1:], **kwargs)
+        return result.get()
+    return wrapper
+
+
+def bind_binary_op(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        # FIXME assump the input tensor is the first positional arguments in
+        # all tf.op design
+        lhs_tensor = MonadicTensor(args[0])
+        rhs_tensor = MonadicTensor(args[1])
+        result = lhs_tensor.bind_bin_op(f, rhs_tensor, *args[2:], **kwargs)
+        return result.get()
+    return wrapper
---
+++ gemini/plugins/monad.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+from functools import reduce
+
+
+class MonadicTensor:
+
+    def __init__(self, value):
+        if isinstance(value, list) or isinstance(value, tf.Tensor):
+            self.value = value
+        elif isinstance(value, MonadicTensor):
+            self.value = value.get()
+        else:
+            assert 0, 'got undefined type {}'.format(type(value))
+
+    def get(self):
+        return self.value
+
+    def __str__(self):
+        return 'MonadicTensor(' + "\n".join(map(str, self.value)) + ')'
+
+    def __or__(self, f):
+        return self.bind(f)
+
+    # def __add__(self, rhs):
+    #     if not isinstance(other, self.__class__):
+    #         if not isinstance(other, (list, tuple)):
+    #             tmp = np.ones(self._shape) * other
+    #             rhs = self.__class__(list(tmp))
+    #         else:
+    #             rhs = self.__class__(other)
+    #     else:
+    #         rhs = other
+    # return self.__class__(list(map(operator.add, self._buffer,
+    # rhs._buffer)))
+
+    def reduce(self, *args, **kwargs):
+        f = args[0]
+        if isinstance(self.value, tf.Tensor):
+            return self
+        elif isinstance(self.value, list):
+            # FIXME, partial appends new args, not work for pending for first undefined args
+            # result = list(map(functools.partial(f, *args[1:], **kwargs), self.value))
+            # legacy code, use lambda function
+            result = reduce(f, self.value)
+            assert isinstance(result, tf.Tensor), 'should be tf.Tensor'
+            return MonadicTensor(result)
+        else:
+            assert 0, 'got undefined type'
+
+    def bind(self, *args, **kwargs):
+        f = args[0]
+        if isinstance(self.value, tf.Tensor):
+            result = f(self.value, *args[1:], **kwargs)
+            return MonadicTensor(result)
+        elif isinstance(self.value, list):
+            # FIXME, partial appends new args, not work for pending for first undefined args
+            # result = list(map(functools.partial(f, *args[1:], **kwargs), self.value))
+            # legacy code, use lambda function
+            result = list(
+                map(lambda inp: f(inp, *args[1:], **kwargs), self.value))
+            return MonadicTensor(result)
+        else:
+            assert 0, 'got undefined type'
+
+    def bind_bin_op(self, *args, **kwargs):
+        f = args[0]
+        rhs_operand = args[1]
+        assert isinstance(self.value, type(rhs_operand.get())), \
+            'bin op found unmatched lhs {} and rhs {}'.format(
+                type(self.value), type(rhs_operand))
+        if isinstance(self.value, tf.Tensor):
+            result = f(self.value, rhs_operand.get(), *args[1:], **kwargs)
+            return MonadicTensor(result)
+        elif isinstance(self.value, list):
+            # FIXME, partial appends new args, not work for pending for first undefined args
+            # result = list(map(functools.partial(f, *args[1:], **kwargs), self.value))
+            result = list(map(lambda lhs, rhs: f(lhs, rhs, *
+                                                 args[2:], **kwargs), self.value, rhs_operand.get()))
+            return MonadicTensor(result)
+        else:
+            assert 0, 'got undefined type'
