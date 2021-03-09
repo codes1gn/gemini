581c384e7774cee7786f091187da300ee4dc9e01
albert.shi <heng.shi@enflame-tech.com>
2021-03-09 18:09:38 +0800

    support dense, with non-sharding mode interface

--- gemini/plugins/bert_plugin.py
+++ gemini/plugins/bert_plugin.py
@@ -36,10 +36,6 @@ def transpose(*args, **kwargs):
 
 @bind_binary_op
 def matmul(*args, **kwargs):
-    print('anchor')
-    print(args[0])
-    print(args[1])
-    # assert 0
     return tf.matmul(*args, **kwargs)
 
 @reduce_unary_op
@@ -57,60 +53,62 @@ def reshape(*args, **kwargs):
     new_shape[-1] = -1
     return tf.reshape(args[0], new_shape, *args[2:], **kwargs)
 
-# if not _dense_sharding_switch:
-#     @bind_unary_op
-#     def dense(*args, **kwargs):
-#         return tf.layers.dense(*args, **kwargs)
+if not _dense_sharding_switch:
+    print('anchor')
+    @bind_unary_op
+    def dense(*args, **kwargs):
+        return tf.layers.dense(*args, **kwargs)
             
-def dense(*args, **kwargs):
-    if not _dense_sharding_switch:
-        # do not shard weights
-        if isinstance(args[0], tf.Tensor):
-            return tf.layers.dense(*args, **kwargs)
-        elif (isinstance(args[0], tuple) or isinstance(args[0], list)) \
-                and isinstance(args[0][0], tf.Tensor):
-            # TODO wrap this
-            _ret = []
-            _rename_idx = 0
-            _name_base = kwargs['name'] + "_"
-            for _input_tensor in args[0]:
-                kwargs['name'] = _name_base + str(_rename_idx)
-                _rename_idx += 1
-                print(kwargs['name'])
-                _ret.append(tf.layers.dense(
-                    _input_tensor, *args[1:], **kwargs))
-            assert isinstance(_ret, list)
-            return _ret
-
-    elif _dense_sharding_switch:
-        sharded_shape = args[1] // _sharding_size
-        if isinstance(args[0], tf.Tensor):
-            _ret = []
-            _rename_idx = 0
-            _name_base = kwargs['name'] + "_"
-            for _idx in range(_sharding_size):
-                kwargs['name'] = _name_base + str(_rename_idx)
-                _rename_idx += 1
-                _ret.append(tf.layers.dense(
-                    args[0], sharded_shape, *args[2:], **kwargs))
-            assert isinstance(_ret, list)
-            return _ret
-
-        elif (isinstance(args[0], tuple) or isinstance(args[0], list)) \
-                and isinstance(args[0][0], tf.Tensor):
-            _ret = []
-            _rename_idx = 0
-            _name_base = kwargs['name'] + "_"
-            for _i_tensor in args[0]:
-                kwargs['name'] = _name_base + str(_rename_idx)
-                _rename_idx += 1
-                _ret.append(tf.layers.dense(
-                    _i_tensor, sharded_shape, *args[2:], **kwargs))
-            # replace by reduce
-            _ret_tensor = tf.add_n(_ret)
-            assert isinstance(_ret_tensor, tf.Tensor)
-            return _ret_tensor
-
-    else:
-        assert 0, 'expected tf.Tensor or list/tuple of tf.Tensor as inputs, but got {}'.format(
-            type(input_symbol))
+# def dense(*args, **kwargs):
+#     if not _dense_sharding_switch:
+#         # do not shard weights
+#         if isinstance(args[0], tf.Tensor):
+#             return tf.layers.dense(*args, **kwargs)
+#         elif (isinstance(args[0], tuple) or isinstance(args[0], list)) \
+#                 and isinstance(args[0][0], tf.Tensor):
+#             # TODO wrap this
+#             _ret = []
+#             _rename_idx = 0
+#             _name_base = kwargs['name'] + "_"
+#             for _input_tensor in args[0]:
+#                 kwargs['name'] = _name_base + str(_rename_idx)
+#                 _rename_idx += 1
+#                 print(kwargs['name'])
+#                 _ret.append(tf.layers.dense(
+#                     _input_tensor, *args[1:], **kwargs))
+#             assert isinstance(_ret, list)
+#             return _ret
+# 
+#     elif _dense_sharding_switch:
+#         sharded_shape = args[1] // _sharding_size
+#         if isinstance(args[0], tf.Tensor):
+#             _ret = []
+#             _rename_idx = 0
+#             _name_base = kwargs['name'] + "_"
+#             for _idx in range(_sharding_size):
+#                 kwargs['name'] = _name_base + str(_rename_idx)
+#                 _rename_idx += 1
+#                 _ret.append(tf.layers.dense(
+#                     args[0], sharded_shape, *args[2:], **kwargs))
+#             assert isinstance(_ret, list)
+#             return _ret
+# 
+#         elif (isinstance(args[0], tuple) or isinstance(args[0], list)) \
+#                 and isinstance(args[0][0], tf.Tensor):
+#             _ret = []
+#             _rename_idx = 0
+#             _name_base = kwargs['name'] + "_"
+#             for _i_tensor in args[0]:
+#                 kwargs['name'] = _name_base + str(_rename_idx)
+#                 _rename_idx += 1
+#                 _ret.append(tf.layers.dense(
+#                     _i_tensor, sharded_shape, *args[2:], **kwargs))
+#             # replace by reduce
+#             _ret_tensor = tf.add_n(_ret)
+#             assert isinstance(_ret_tensor, tf.Tensor)
+#             return _ret_tensor
+# 
+#     else:
+#         assert 0, 'expected tf.Tensor or list/tuple of tf.Tensor as inputs, but got {}'.format(
+#             type(input_symbol))
+# 
